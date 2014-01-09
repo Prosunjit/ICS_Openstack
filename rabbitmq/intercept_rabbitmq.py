7899ab5b322b8aef26ee9f39047fc52f3807aa35
Prosunjit Biswas <prosun.csedu@gmail.com>
2014-01-08 20:33:08 -0600

    it can intercept all event happening in openstack

--- rabbitmq/intercept_rabbitmq.py
+++ rabbitmq/intercept_rabbitmq.py
@@ -9,7 +9,7 @@
 import pika
 import sys
 import json
-from filter import conf
+from filter import conf_path, conf_value
 
 from jsonpath_rw import jsonpath, parse
 #import MessageFilter
@@ -48,20 +48,38 @@ class MessageFilter:
 
     def parse_from_file(self):
         pass
-    def parse_from_config(self,config):
+    def parse_from_config(self,config, config_values):
         # config is a dictionary
-        config_values = {}
+        retrieve_values = {}
         for conf in config:
-            config_values[conf] = self.parse_from_path(config[conf])
+	    value = self.parse_from_path(config[conf])
+	    if (config_values[conf].lower() == "any" ) or (value and value[0] == config_values[conf]):
+	      retrieve_values[conf] = value
+            
 
-        return config_values
+        return retrieve_values
     '''
         The json path to retrieve value from can be given as a config file, or as a dictionary containing paths
     '''
     def parse(self):
-        return self.parse_from_config(conf)
+        return self.parse_from_config(conf_path, conf_value)
 
-
+def exchange_exists(exchange_name,exchg_type):
+    print "exchange name is " + exchange_name
+    if exchange_name.__len__() == 0:
+      return False    
+    parameters = pika.URLParameters('amqp://guest:admin@localhost:5672/%2F')
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    try:   
+      print channel.exchange_declare(exchange=exchange_name, exchange_type=exchg_type, passive=True).method
+      if ( channel.exchange_declare(exchange=exchange_name, exchange_type=exchg_type, passive=True).method ):      
+	  return True
+      else:	  
+	  return False
+    except:	      
+	  #print sys.exc_info()[0]
+	  return False      
 
 class OpenRabbit:
     messageno = 0
@@ -87,21 +105,41 @@ class OpenRabbit:
                       no_ack=True)
 
         channel.start_consuming()
+        
+        
 
     def nativecall(self, message):
-        print MessageFilter(message=message).parse()
+        
+        msg_params = MessageFilter(message=message).parse()
+        print msg_params 
+        #print msg_params['request_id'][0][4:]
+        if exchange_exists(msg_params['msg_id'][0],"direct") == True :
+	    print "----------exchange already exists-----------"+ msg_params['msg_id']
+	    exit
+	    
+	if exchange_exists(msg_params['request_id'][0][4:],"direct") == True :	  
+	    print "*********************exchange already exists-----------"+ msg_params['request_id']
+	    exit 
+	    
 
     def callback(self, ch, method, properties, body):
 
         self.messageno = self.messageno + 1
         print "\n\n"
         print ("----------------{}th message -----------------\n".format(self.messageno))
-        print method.routing_key
-        self.nativecall(body)
+        print method.routing_key+"\n"
+        print body
+        
+        #self.nativecall(body)
         #print body
         #print MessageFilter(body).parse()
+        
 
 def open_rabbit_test():
     OpenRabbit()
+    
+  
 
 open_rabbit_test()
+
+    
